02eba0e8d88c385a32b8269ea4e4b0bdd2a0c53c
Florian Roudaut <florian.roudaut@gmail.com>
2023-01-29 21:30:52 +0100

    FirstCommit

---
+++ PersonalAccounting.Client/Shared/DataStore.cs
@@ -0,0 +1,4 @@
+public static class DataStore
+{
+    public static List<Flow> Flows = new List<Flow>();
+}
---
+++ PersonalAccounting.Client/Shared/Flow.cs
@@ -0,0 +1,8 @@
+public class Flow
+{
+    public string Name {get;set;}
+
+    public DateTime Date {get;set;}
+
+    public double Amount {get;set;}
+}
